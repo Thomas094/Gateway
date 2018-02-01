f694ff213e0ae26485d9c779fbf4e4d0eccea09c
byMyWay <orglanss@gmail.com>
2018-02-01 10:54:53 +0800

    test

--- keepconnection.sh
+++ keepconnection.sh
@@ -2,20 +2,20 @@
 # File              : keepconnection.sh
 # Author            : orglanss <orglanss@gmail.com>
 # Date              : 22.09.2017
-# Last Modified Date: 08.11.2017
+# Last Modified Date: 01.02.2018
 # Last Modified By  : orglanss <orglanss@gmail.com>
 # This script try to keep the connection to the internet
 # NOTE              : Run with sudo
 
 
 IP="115.156.156.183"
+UPLOAD_SUCCESS=true
 # reconnect internet and output ipaddress using stdout 
 function reconnect(){
 mentohust -k1 -b3
 # wait for connection finished
-sleep 2
-cat /tmp/mentohust.log|grep '使用IP'|\
-grep -o '[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*'|sed -n '2p'
+sleep 10
+cat /tmp/mentohust.log|grep '使用IP'|grep -o '[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*'|sed -n '2p'
 }
 
 # upload ipaddress to github
@@ -28,21 +28,28 @@ ipaddr=$1
 while ((1));
 do
 {
-# test connection stat
+  # test connection stat
   connected=$(ping -w 4 baidu.com 2>&1| grep "100% packet loss\|unknown host" ); 
   if [ ! -z "${connected}" ]; then
     echo "reconnecting ...."
-    newIP=`reconnect`
-    # if ip changed, upload the new ip address to github
-    if [ ! -z ${newIP} ] && [ ${newIP} != ${IP} ];then
-      echo Current IP:${newIP}
-      IP=${newIP}
-      upload ${IP}
-    fi      
-  else
-    echo "connection is good"
+    curIP=`reconnect`
+    while [ -z ${curIP}]
+    do
+      # get ip failed, retry 
+      curIP=`reconnect`
+    done
   fi
-# wait for a minute befor test connection again
+  # if ip changed, upload the new ip address to github
+  if [ ! -z ${newIP} ] && [ ${newIP} != ${IP} ];then
+    echo Current IP:${newIP}
+    IP=${newIP}
+    upload ${IP}
+  fi      
+  # if upload failed, retry
+  if [ ! ${UPLOAD_SUCCESS} ]; then
+    upload ${IP}
+  fi
+  # wait for a minute befor test connection again
   sleep 60
 }
 done
